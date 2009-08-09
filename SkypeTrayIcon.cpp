19f859d40bf113bacdb0ce138bd98f2547d6eb45
Johann Uhrmann <johann.uhrmann@gmx.de>
2009-08-09 15:39:41 +0200

    Changed license to GPLv3

--- SkypeTrayIcon.cpp
+++ SkypeTrayIcon.cpp
@@ -1,8 +1,21 @@
 /*
-	Programm to indicate unsent skype messages in the system tray.
-	J. Uhrmann, 08.2009
+    Copyright Johann Uhrmann 2009
 
-	All rights reserved.
+    This file is part of SkypeMessageStatus.
+
+    SkypeMessageStatus is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    SkypeMessageStatus is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with SkypeMessageStatus.  If not, see <http://www.gnu.org/licenses/>.
+
 */
 
 #include "SkypeTrayIcon.h"
